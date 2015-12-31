2f14fd973044711364e11be08aa6030af3fc1c23
Ryan Lijewski <RLijewski@users.noreply.github.com>
2015-12-31 15:05:55 -0600

    Added Members page. Added carousel with dummie pictures to members page

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,4 @@
+class MembersController < ApplicationController
+	def members
+  	end  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
     get :about
   end
 
+  controller :members do
+    get :members
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
