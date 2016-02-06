856dc3799fef365575a30de254554dfc22738ef7
Ryan Lijewski <RLijewski@users.noreply.github.com>
2016-02-06 07:03:25 -0600

    Cleaned up custom.js, fixed typo in topics banner.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,29 +1,3 @@
-//$(document).ready(function() {
-//
-//    //Change style of navbar on different pages
-//    if ($("div").hasClass("about")) {
-//        $('.navbar-inverse').css('background-color', 'rgba(0,0,0,.4)');
-//        $('body').css('padding-top', '0px');
-//        $('.navbar-inverse .navbar-nav li a').css('color', '#eee');
-//    }
-//
-//    if ($("div").hasClass("resources")) {
-//        $('.navbar-inverse').css('background-color', '#5BC0DE');
-//        $('.navbar-inverse .navbar-nav li a').css('color', '#eee');
-//        $('.intro').css('background-color', '#5BC0DE');
-//        $('.btn-info').css('background-color', '#5BC0DE');
-//        $('.card').css('color', '#5BC0DE');
-//    }
-//
-//    if ($("div").hasClass("topics")) {
-//        $('.navbar-inverse').css('background-color', '#00cc44');
-//        $('.navbar-inverse .navbar-nav li a').css('color', '#eee');
-//        $('.intro').css('background-color', '#00cc44');
-//        $('.btn-info').css('background-color', '#00cc44');
-//        $('.card').css('color', '#00cc44');
-//    }
-//
-//});
 
 //Back to top button
 var offset = 40;
@@ -38,7 +12,7 @@ $(window).scroll(function() {
 
     $(".up").click(function(e) {
         e.preventDefault();
-            $("html, body").animate({scrollTop: 0}, duration).finish();
+        $("html, body").animate({scrollTop: 0}, duration).finish();
     });
 });
 
